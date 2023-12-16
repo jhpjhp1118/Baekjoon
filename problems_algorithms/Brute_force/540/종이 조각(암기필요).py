65b1493824f0632031f69f568f28bb8c1784bc0d
Joon-hyuk Park <jhpjhp1118@gmail.com>
2023-12-16 16:15:59 +0900

    update

--- problems_algorithms/Brute_force/540/종이 조각(암기필요).py
+++ problems_algorithms/Brute_force/540/종이 조각(암기필요).py
@@ -1,6 +1,7 @@
 # https://www.acmicpc.net/problem/14391
 import sys
 # 참고 링크: https://lagooni.tistory.com/entry/%EB%B0%B1%EC%A4%80-%EC%A2%85%EC%9D%B4-%EC%A1%B0%EA%B0%81-14391%EB%B2%88-Python-Bitmasking
+# 더 좋은 참고 링크: https://vixxcode.tistory.com/23
 n, m = list(map(int, sys.stdin.readline().strip().split()))
 
 paper = [list(map(int, list(sys.stdin.readline().strip()))) for _ in range(n)]
@@ -10,8 +11,8 @@ ans = 0
 # i번째 bitmask grid에 대해 탐색한다.
 # 각 칸마다 0 & 1 2가지 값을 가질 수 있고, grid는 n*m 칸을 가지므로, 총 2^(n*m) 개의 경우를 탐색한다
 # 2진법의 비트연산자를 쓰는 이유: 0과 1로만 이루어진 n*m개의 칸을 가지는 2진수가 있을 때, 이를 n by m 행렬로 바꾸면 grid가 된다.
-# Ex) 000011 --> 000 / 011
-# bitmask 규칙은 다음과 같이 정한다. --> 세로합을 해야되는 칸: 1, 가로합을 해야되는 칸: 0
+# Ex) 000011 --> 110 / 000
+# bitmask 규칙은 다음과 같이 정한다. --> 가로합을 해야되는 칸: 1, 세로합을 해야되는 칸: 0
 for i in range(1 << n*m):
     total = 0
     # i번째 경우의 가로합만 전부 계산해서 total에 더해주기
